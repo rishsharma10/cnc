2cd9b36f455e1cc3239e4650658dc091b2e82120
Abhay <abhay.singh.henceforth@gmail.com>
2024-12-28 15:14:34 +0530

    header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -70,7 +70,44 @@ const Home = () => {
   ]
   return (
     <>
-
+      <nav className="navbar navbar-expand-lg bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src={logo.src} alt="error" height={60} width={60} />
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul className="navbar-nav mb-2 ms-auto gap-4 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Page</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link "  href="#">Portofolio</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link "  href="#">Blog</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link "  href="#">Shop</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link "  href="#">Element</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link "  href="#"><i className="fa-solid fa-magnifying-glass"></i></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link "  href="#"><i className="fa-solid fa-grip-lines"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       {/* ------------------------- Hero Section ------------------------- */}
       <section className="hero-section py-0 h-100">
         <div className="container-fluid h-100">
